5e74b6b31b7adf1cbe425ac40e4c8091cb6f4d56
nruest <ruestn@gmail.com>
2015-04-29 16:41:21 -0400

    Create download link to ORIGINAL_PDF dsid if it is available. YUDL - Buddhism Across Boundaries - #380

--- theme/islandora-book-book.tpl.php
+++ theme/islandora-book-book.tpl.php
@@ -15,6 +15,12 @@
     <?php print $description; ?>
     <?php if($parent_collections): ?>
   <div>
+    <?php if (isset($islandora_book)): ?>
+      <h2><?php print t('Download'); ?></h2>
+        <ul>
+          <li>PDF: <?php print $islandora_book; ?>
+        </ul>
+       <?php endif; ?>
     <h2><?php print t('In collections'); ?></h2>
     <ul>
       <?php foreach ($parent_collections as $collection): ?>
